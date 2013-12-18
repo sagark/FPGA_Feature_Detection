b7eb73988996788868887cccf51daa92a5f3fc8c
Austin Buchan <austin.d.buchan@gmail.com>
2013-12-18 13:23:12 -0800

    Adding license information

--- hardware/src/vga/VGA.v
+++ hardware/src/vga/VGA.v
@@ -1,3 +1,56 @@
+//==============================================================================
+//  File:   VGA.v
+//  Author: Austin Buchan (abuchan@eecs.berkeley.edu)
+//          Adapted from work by Anthony Demarco, Loring Scotty Hoag, CS150 F09
+//  Copyright:  Copyright 2005-2014 UC Berkeley
+//  Version: Updated for UC Berkeley CS150 Fall 2013 Course
+//==============================================================================
+
+//==============================================================================
+//  Section:  License
+//==============================================================================
+//  Copyright (c) 2005-2014, Regents of the University of California
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//    - Redistributions of source code must retain the above copyright notice,
+//      this list of conditions and the following disclaimer.
+//    - Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer
+//      in the documentation and/or other materials provided with the
+//      distribution.
+//    - Neither the name of the University of California, Berkeley nor the
+//      names of its contributors may be used to endorse or promote
+//      products derived from this software without specific prior
+//      written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+//==============================================================================
+
+//------------------------------------------------------------------------------
+//  Module: VGA
+//
+//  Desc: Interfaces with AD9980 VGA capture chip to produce 800x600 video
+//    information. Converts RGB to average grayscale value. Known to glitch 
+//    occasionally on vertical sync. Depends on correct I2C initializtion of
+//    capture device (see DVIInitial)
+//
+//  Params: None
+//------------------------------------------------------------------------------
+
 module VGA (
   input reset,
   output clock,
